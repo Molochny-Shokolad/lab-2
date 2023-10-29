c3efbb752cd2d971690819bcb4baf33c389032aa
ercan1104 <saidgasanov8@gmail.com>
2023-10-29 18:23:53 +0300

    Lab 2.1, 2.3

---
+++ Lab 2.3/Lab 2.3.ino
@@ -0,0 +1,44 @@
+#define WIFI_SSID "**********"
+#define WIFI_PASS "**********"
+#define BOT_TOKEN "***********************************************"
+#include <FastBot.h>
+FastBot bot(BOT_TOKEN);
+void setup() {
+    connectWiFi();
+    bot.attach(newMsg);
+}
+void newMsg(FB_msg& msg) {
+    String tem = "("+msg.chatID+", "+msg.username+", "+msg.text+")";
+    if(msg.text == "Hello") {
+        String otp = "Привет!";
+        bot.sendMessage(otp, msg.chatID);
+        Serial.println(tem+otp);
+    }
+    Serial.println(tem);
+}
+void loop() {
+    bot.tick();
+}
+void connectWiFi() {
+    delay(2000);
+    Serial.begin(115200);
+    Serial.println();
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        if (millis() > 15000) ESP.restart();
+    }
+    Serial.println("Connected");
+}
+// В данном коде изменилась функция "void newMsg(FB_msg& msg)":
+void newMsg(FB_msg& msg) {
+    String tem = "("+msg.chatID+", "+msg.username+", "+msg.text+")";
+    if(msg.text == "Hello") {
+        String otp = "Привет!";
+        bot.sendMessage(otp, msg.chatID);
+        otp = "На сообщение отправить == "+otp;
+        Serial.println(tem+otp);
+    }
+    Serial.println(tem);
+}
